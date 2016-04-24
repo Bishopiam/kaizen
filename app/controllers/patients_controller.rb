e46d42082677753cd51cf208c573fdc8d5c470a9
Bishopiam <nehemiahbishop@gmail.com>
2016-04-24 14:23:20 -0500

    working index and show actions

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,4 +3,10 @@ class PatientsController < ApplicationController
 		@patients = Patient.all
 		render 'index.html.erb'
 	end
+
+	def show
+		patient_id = params[:id]
+		@patient = Patient.find_by(id: patient_id)
+		render 'show.html.erb'
+	end
 end
